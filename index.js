2aaa718681cf7ea20b3ec35efa2a2fc5d2f9bdb7
Pedro Wanderley <peu-zico>
2019-10-30 17:38:00 -0300

    Adding middlewares

--- index.js
+++ index.js
@@ -6,17 +6,38 @@ server.use(express.json());
 
 const users = ['Diego', 'Pedro', 'Henrique']
 
+server.use((req, res, next) => {
+  console.log('A requisição foi chamada');
+  next();
+});
+
+function checkUserExists(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: 'User name is requerid' });
+  }
+  return next();
+};
+
+function checkUserInArray(req, res, next) {
+  const user = users[req.params.index];
+  if (!user) {
+    return res.status(400).json({ error: 'User not found' });
+  }
+
+  req.user = user;
+
+  return next();
+};
+
 server.get('/users', (req, res) => {
   return res.json({users});
 });
 
-server.get('/users/:index', (req, res) => {
-  const {index} = req.params;
-
-  return res.json({ message: `${users[index]}` });
+server.get('/users/:index', checkUserInArray, (req, res) => { 
+  return res.json(req.user);
 });
 
-server.post('/users', (req, res) => {
+server.post('/users', checkUserExists, (req, res) => {
   const {name} = req.body;
 
   users.push(name);
@@ -24,7 +45,7 @@ server.post('/users', (req, res) => {
   return res.json( users );
 });
 
-server.put('/users/:index', (req, res) => {
+server.put('/users/:index', checkUserInArray , checkUserExists, (req, res) => {
   const {index} = req.params;
   const {name} = req.body;
 
@@ -33,7 +54,7 @@ server.put('/users/:index', (req, res) => {
   return res.json(users);
 });
 
-server.delete('/users/:index', (req, res) => {
+server.delete('/users/:index', checkUserInArray , (req, res) => {
   const { index } = req.params;
 
   users.splice(index, 1);

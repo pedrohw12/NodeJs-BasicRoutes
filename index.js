3488be50d6e6c9fdcf710442984fa0e4835db549
Pedro Wanderley <peu-zico>
2019-10-30 10:32:31 -0300

    Making CRUD

--- index.js
+++ index.js
@@ -2,8 +2,43 @@ const express = require('express');
 
 const server = express();
 
-server.get('/teste', (req, res)=>{
-   res.json({message:'Hello mundo'});
+server.use(express.json());
+
+const users = ['Diego', 'Pedro', 'Henrique']
+
+server.get('/users', (req, res) => {
+  return res.json({users});
+});
+
+server.get('/users/:index', (req, res) => {
+  const {index} = req.params;
+
+  return res.json({ message: `${users[index]}` });
+});
+
+server.post('/users', (req, res) => {
+  const {name} = req.body;
+
+  users.push(name);
+
+  return res.json( users );
+});
+
+server.put('/users/:index', (req, res) => {
+  const {index} = req.params;
+  const {name} = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+  return res.send();
 });
 
 server.listen(3000);
+
